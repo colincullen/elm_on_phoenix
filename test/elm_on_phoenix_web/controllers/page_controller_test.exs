98f5fe5c881e21c3572528382dc931894be667f0
colincullen <colincullen@embarqmail.com>
2019-02-01 00:07:22 -0500

    Minimal Elm on Phoenix implementation

--- test/elm_on_phoenix_web/controllers/page_controller_test.exs
+++ test/elm_on_phoenix_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ElmOnPhoenixWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "elm-container"
   end
 end
